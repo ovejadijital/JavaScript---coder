31c54e6f6b4f98f413d08759cd255399bb3467a2
nacor taboada <taboadanacor2033@gmail.com>
2023-10-17 19:09:04 -0300

    primera entrega

---
+++ main.js
@@ -0,0 +1,37 @@
+//variables
+
+let nombre = "pablo";
+const apellido = "perez";
+let edad = 30;
+const sexo = "masculino";
+
+//condicional
+
+const var1 = "funciona";
+const var2 = "en serio";
+
+if (true){ 
+ console.log(var1);
+}
+
+if (false) {
+    console.log(var2);
+}
+
+//bucles
+
+for (let i = 0; i < 10; i = i + 1) {
+    console.log(i);
+
+}
+console.log("fin");
+
+//funcion
+
+function pedirNombre () {
+    const name = prompt ("ingrese su nombre" );
+    console.log( nombre + name);
+
+}
+
+perdirnombre();
