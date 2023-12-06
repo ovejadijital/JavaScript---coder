caa75c27eec2132a0538dd92ae1a5cdd39a7effe
nacor taboada <taboadanacor2033@gmail.com>
2023-12-06 16:22:23 -0300

    actualizacion

--- main.js
+++ main.js
@@ -1,236 +1,186 @@
-  class Asiento {
-
-  constructor (id, precio = 20) {
-  this.id = id;
-  this.comprado = true;
-  this.seleccionado = false;
-  this.precio = precio;
+class Asiento {
+  constructor(id, precio = 20) {
+    this.id = id;
+    this.comprado = false;
+    this.seleccionado = false;
+    this.precio = precio;
   }
-
-  }
-
-  function seteartotalasientosseleccionados(asientosseleccionados) {
-    const spantotalasientosseleccionados = document.getElementById("asientosseleccionados");
-
-    if (spantotalasientosseleccionados) {
-        const totalasientosseleccionados = asientosseleccionados.reduce((acc, el) => acc + el.precio, 0);
-        spantotalasientosseleccionados.innerText = `$${totalasientosseleccionados}`;
-    } else {
-        console.error("No se encontró el elemento con ID 'asientosseleccionados'");
-    }
 }
 
+function seteartotalasientosseleccionados(asientosseleccionados) {
+  const spantotalasientosseleccionados = document.getElementById(
+    "asientosseleccionados"
+  );
 
+  if (spantotalasientosseleccionados) {
+    const totalasientosseleccionados = asientosseleccionados.reduce(
+      (acc, el) => acc + el.precio,
+      0
+    );
+    spantotalasientosseleccionados.innerText = `$${totalasientosseleccionados}`;
+  } else {
+    console.error("No se encontró el elemento con ID 'asientosseleccionados'");
+  }
+}
 
-  function comprarasientos(asiento) {
-      asiento.comprado = true;
+let listadeasientos = [];
 
-      const divasiento = document.getElementById(`asiento-${asiento.id}`);
-      divasiento.classList.remove("seleccionado");
-      divasiento.classList.add("comprado");
+function buscarasientoenlista(id) {
+  let asientoencontrado = null;
+
+  for (const asiento of listadeasientos) {
+    if (asiento && asiento.id === id) {
+      asientoencontrado = asiento;
+    }
   }
 
-  function buscarasientoenlista(id) {
-      
+  return asientoencontrado;
+}
 
-      let asientoencontrado = null;
+function obtenerasientoscomprados() {
+  const asientosComprados =
+    JSON.parse(localStorage.getItem("asientoscomprados")) || [];
 
-      for (const asiento of listadeasientos) {
-          
-        if (asiento && asiento.id === id) {
-          asientoencontrado = asiento;
-        }  
-      }
-
-      return asientoencontrado;
-
-  }
-
-
-  function obtenerasientoscomprados() {
-      const asientosComprados = JSON.parse(localStorage.getItem("asientoscomprados")) || [];
   for (const asientocomprado of asientosComprados) {
-      const asiento = buscarasientoenlista(asientocomprado.id);
-      
-      if (asiento !== null) {
-          
-        asiento.comprado = true;
+    const asiento = buscarasientoenlista(asientocomprado.id);
 
-      }
-
+    if (asiento !== null) {
+      asiento.comprado = true;
+    }
   }
+}
+
+let asientosComprados = [];
+
+function comprarasientos() {
+  const asientoscompradosyseleccionados = [
+    ...asientosComprados,
+    ...asientosseleccionados,
+  ];
+
+  for (const asientoseleccionado of asientosseleccionados) {
+    asientoseleccionado.comprado = true;
   }
 
-  let asientosComprados = [];
+  asientosseleccionados = [];
 
-  function comprarasientos() {
-      
-      const asientoscompradosyseleccionados = [
-          ...asientosComprados,
-          ...asientosseleccionados,
-      ];
+  localStorage.setItem(
+    "asientoscomprados",
+    JSON.stringify(asientoscompradosyseleccionados)
+  );
 
-        for (const asientoseleccionado of asientosseleccionados) {
-          asientoseleccionado.comprado = true;
-          
-        }
-
-        asientosseleccionados = [];
-
-      localStorage.setItem("asientoscomprados", JSON.stringify(asientoscompradosyseleccionados));
-
-      obtenerasientoscomprados();
+  obtenerasientoscomprados();
 
   seteartotalasientosseleccionados(asientosseleccionados);
-
-  }
-
-  function indiceasientoseleccionado(id) {
-    return asientosseleccionados.findIndex((el) => {
-        return el.id === id;
-    });
 }
 
-  function seleccionarasiento(asiento) {
-      if (asiento.comprado) {
-          Swal.fire({
-              title: 'asiento no disponible',
-              text: 'este asiento esta comprado',
-              icon: 'error',
-              confirmButtonText: 'ok'
-            })
-          return;
-      }
+function indiceasientoseleccionado(id) {
+  return asientosseleccionados.findIndex((el) => el.id === id);
+}
 
-
-    
-      divasiento.classList.add("seleccionado");
-
-      const indexasientoseleccionado = indiceasientoseleccionado(Asiento.id);
-
-      if (indexasientoseleccionado !== -1) {
-        
-          asiento.seleccionado = false;
-          divasiento.classList.remove("seleccionado");
-
-            asientosseleccionados.splice(indexasientoseleccionado, 1);
-      } else{
-          asiento.seleccionado = true;
-
-        divasiento.classList.add("seleccionado");
-        asientosseleccionados.push(asiento);
-    
-      }
-
-  
+function seleccionarasiento(asiento) {
+  if (asiento.comprado) {
+    Swal.fire({
+      title: "asiento no disponible",
+      text: "este asiento está comprado",
+      icon: "error",
+      confirmButtonText: "ok",
+    });
+    return;
   }
 
-  function renderizarasientos(asientos) {
-      const divasientos = document.getElementById("asientos");
-
-  divasientos.innerHTML = "";
-
-
-  for(const filadeasientos of asientos){
-
-  const divflex = document.createElement("div");
-  divflex.className = "d-flex align-items-center";
-
-  for(const asiento of filadeasientos) {
-
-
-    const divasiento = document.createElement("div");
-    divasiento.className = "asiento";
-    
+  const divasiento = document.getElementById(`asiento-${asiento.id}`);
 
   if (asiento !== null) {
-      divasiento.className += "seleccionable";
-      divasiento.id = asiento-$;{asiento};
+    divasiento.classList.add("seleccionado");
 
-  if (asiento.comprado) {
-  divasiento.className += "comprado";   
+    const indexasientoseleccionado = indiceasientoseleccionado(asiento.id);
 
-
+    if (indexasientoseleccionado !== -1) {
+      asiento.seleccionado = false;
+      divasiento.classList.remove("seleccionado");
+      asientosseleccionados.splice(indexasientoseleccionado, 1);
+    } else {
+      asiento.seleccionado = true;
+      divasiento.classList.add("seleccionado");
+      asientosseleccionados.push(asiento);
+    }
   }
+}
 
-  if (asiento.seleccionado) {
-      divasiento.className += "seleccionado";
-  }
+function renderizarasientos(asientos) {
+  const divasientos = document.getElementById("asientos");
+  divasientos.innerHTML = "";
 
-  divasiento.addEventListener("click", () => {
-      seleccionarasiento(asiento);
-  });
+  for (const filadeasientos of asientos) {
+    const divflex = document.createElement("div");
+    divflex.className = "d-flex align-items-center";
 
-  }
+    for (const asiento of filadeasientos) {
+      const divasiento = document.createElement("div");
+      divasiento.className = "asiento";
 
+      if (asiento !== null) {
+        divasiento.className += " seleccionable";
+        divasiento.id = `asiento-${asiento.id}`;
 
-  divflex.append(divasiento);
+        if (asiento.comprado) {
+          divasiento.className += " comprado";
+        }
 
+        if (asiento.seleccionado && !asiento.comprado) {
+          divasiento.className += " seleccionado";
+        }
+
+        divasiento.addEventListener("click", () => {
+          seleccionarasiento(asiento);
+        });
+      }
+
+      divflex.append(divasiento);
     }
 
-
     divasientos.append(divflex);
-
   }
+}
 
 
-  }
+function obtenerasientosJSON(params) {
+  return new Promise((resolve, reject) => {
+    asientos.length = 0; 
 
-
-  function obtenerasientosJSON(params) {
-      return new Promise((resolve, reject) => {
-          fetch('./asientos.json').then( (Response) => {
-              return Response.json()
-      
-          }).then( (Responsejson) => {
-            
-            for(const filadeasientos of Responsejson){
-              
-              const fila = [];
-      
-              for(const asiento of filadeasientos){
-                  
-                  if (asiento !== "") {
-                      fila.push(new Asiento(asiento));
-                  } else {
-                    fila.push(null);
-                  
-                  }
-      
-      
-              }
-              asientos.push(fila);
-      
+    fetch("./asientos.json")
+      .then((Response) => {
+        return Response.json();
+      })
+      .then((Responsejson) => {
+        for (const filadeasientos of Responsejson) {
+          const fila = [];
+          for (const asiento of filadeasientos) {
+            if (asiento !== "") {
+              fila.push(new Asiento(asiento));
+            } else {
+              fila.push(null);
             }
-            
-            resolve(asientos);
-
-
-          });
-      
-    
+          }
+          asientos.push(fila);
+        }
+        resolve(asientos);
       });
-      
-  }
+  });
+}
 
+let asientosseleccionados = [];
+const asientos = [];
 
+const spantotalasientosseleccionados = document.getElementById(
+  "asientosseleccionados"
+);
+const botoncomprarasientos = document.getElementById("comprarasientos");
 
-  let asientosseleccionados = [];
-  const asientos = [];
+botoncomprarasientos.addEventListener("click", comprarasientos);
 
-  const spantotalasientosseleccionados = document.getElementById("totalasientosseleccionados");
-
-  const botoncomprarasientos = document.getElementById("comprarasientos");
-
-  
-
-  
-    
-    botoncomprarasientos.addEventListener("click", comprarasientos);
-  
-    obtenerasientosJSON().then(() => {
-      obtenerasientoscomprados();
-      renderizarasientos(asientos);
-    });
-
-  
+obtenerasientosJSON().then(() => {
+  obtenerasientoscomprados();
+  renderizarasientos(asientos);
+});
